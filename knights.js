9330534e0cb173fcd8a531c7999c892035911fed
LiamJPOS <paledeepened@gmail.com>
2025-05-20 17:43:16 +0100

    Add BFS algorithm for finding shortest path

--- knights.js
+++ knights.js
@@ -27,5 +27,31 @@ for (let row = 0; row < 8; row++) {
   }
 }
 
+function getShortestRoute(startVertex, targetVertex) {
 
+  let queue = [[startVertex]];
+  let visited = new Set();
+  visited.add(startVertex);
 
+  while (queue.length > 0) {
+    let path = queue.shift();
+    let current = path[path.length - 1] //Current vertex
+
+    if (current === targetVertex) {
+      return path;
+    }
+
+    for (let neighbour of boardGraph[current]) {
+      if (!visited.has(neighbour)) {
+        visited.add(neighbour);
+        queue.push([...path, neighbour]);
+        
+      }
+    }
+  }
+
+  return null
+}
+
+const route = getShortestRoute('0,0', '0,3')
+console.log(route)
